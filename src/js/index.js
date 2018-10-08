9dca07899058449b39648b61c6b11b9a2b95e9fc
ljh <ljh971003@163.com>
2018-10-08 09:47:43 +0800

    考试结束

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// var xml = new XMLHttpRequest();
+// xml.onload = function(res) {
+//     console.log(res.response)
+// }
+// xml.open("get", './data/data.json', true);
+var sec = document.getElementsByClassName('.section');
+sec.onclick = function() {
+    console.log(1)
+}
